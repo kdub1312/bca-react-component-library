7ee6b8f626e1cff2fde257aa8f0fedf74991a52b
kevin <kwagner1312@gmail.com>
2025-09-09 05:53:18 -0500

    adds first component file: button.jsx

---
+++ button.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'; // Not needed in React 17+import React from 'react';
+
+export default function ReactButton() {
+  return (
+    <>
+      <button>click here</button>
+    </>
+  );
+}
