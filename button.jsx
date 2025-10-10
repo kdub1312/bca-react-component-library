df2d764762f88d866de8cf7ca7578f014f726fe8
kevin <kwagner1312@gmail.com>
2025-10-09 19:36:30 -0500

    removes import react statement and uses Global React object in browser via CDN

--- button.jsx
+++ button.jsx
@@ -1,10 +1,22 @@
-// import React from 'react'; // Not needed in React 17+import React from 'react';
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
 export default function ReactButton({ customClass = [], onSelect, children, ...props }) {
+  //use React.useState instead of import { useState } from 'react';
+  //since we are loading the React from CDN in index.html
+  const [isClicked, setIsClicked] = React.useState(false);
+
+
+  function handleClick(cb) {
+    setIsClicked(!isClicked);
+    if (onSelect) {
+      onSelect();
+    }
+  }
+
+
+
   // Ensure customClass is always an array
   const classNames = ['react-button', 'test-class', ...customClass].join(' ');
 
   return (
-    <button className={classNames} onClick={onSelect} {...props}>{children}</button>
+    <button className={classNames} onClick={handleClick} {...props}>{children}</button>
   );
 }
