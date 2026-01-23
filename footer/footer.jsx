78c7948fda3d81cdcb6337cb529b9f6e5b293d80
kevin <kwagner1312@gmail.com>
2026-01-22 18:13:55 -0600

    passes props to footer component

--- footer/footer.jsx
+++ footer/footer.jsx
@@ -1,9 +1,14 @@
-export default function ReactFooter({ customClass = [] }) {
+export default function ReactFooter({ customClass = [], items = [] }) {
   return (
     <footer className={`${customClass.join(' ')} bg-light text-center text-lg-start`}>
       <div className="text-center p-3">
         © 2026 BCA React Component Library
       </div>
+      <ul className="utility-links d-flex flex-column justify-content-center">
+        {items.map((item) => (
+          <li  key={item.name}><a href={item.link}>{item.name}</a></li>
+        ))}
+      </ul>
     </footer>
   );
 }
