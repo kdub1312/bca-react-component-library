c490bdd041a0513bc9ccf0e21f1d799ce977ee83
kevin <kwagner1312@gmail.com>
2026-02-12 05:52:32 -0600

    imports button to ReactChecklistList

--- checklist-list/checklist-list.jsx
+++ checklist-list/checklist-list.jsx
@@ -1,8 +1,24 @@
+import ReactButton from '../button/button.js';
+
 export default function ReactChecklistList({ customClass, checklists, children }) {
     return checklists.map((checklist) => (
         <div key={checklist.id} className="checklist-row">
             <h3>{checklist.title}</h3>
             <p>{checklist.description}</p>
+            <ReactButton 
+                customClass={['list-button']}
+                name="button"
+                id={`delete-btn-${checklist.id}`}
+                data-checklist-id={checklist.id}
+            >Delete
+            </ReactButton>
+            <ReactButton 
+                customClass={['list-button']}
+                name="button"
+                id={`activate-btn-${checklist.id}`}
+                data-checklist-id={checklist.id}
+            >Activate
+            </ReactButton>
         </div>
     ));
 }
