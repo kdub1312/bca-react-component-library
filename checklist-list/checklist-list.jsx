ec246b7e916c094f8eccd0807a2faf0ca6223066
kevin <kwagner1312@gmail.com>
2026-02-16 05:59:55 -0600

    adds style classes to checklist items lists and buttons

--- checklist-list/checklist-list.jsx
+++ checklist-list/checklist-list.jsx
@@ -12,21 +12,21 @@ export default function ReactChecklistList({ customClass, checklists, children }
                 <h3>{checklist.title}</h3>
                 <p>{checklist.description}</p>
                 {/* loop through checklist items here */}
-                <ul>
+                <ul className="item list-group">
                    
                     {items.map((item, index) => (
-                        <li key={index}>{item}</li>
+                        <li key={index} className="list-group-item">{item}</li>
                     ))}
                 </ul>
                 <ReactButton 
-                    customClass={['list-button']}
+                    customClass={['react-button test-class btn btn-danger']}
                     name="button"
                     id={`delete-btn-${checklist.id}`}
                     data-checklist-id={checklist.id}
                 >Delete
                 </ReactButton>
                 <ReactButton 
-                    customClass={['list-button']}
+                    customClass={['react-button test-class btn btn-danger']}
                     name="button"
                     id={`activate-btn-${checklist.id}`}
                     data-checklist-id={checklist.id}
