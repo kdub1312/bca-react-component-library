0656f94a06e3a064dd40d6a85051df90d71507f4
kevin <kwagner1312@gmail.com>
2026-06-10 20:58:20 -0400

    updates checklist-edit and checklist-list components to work with objects

--- checklist-list/checklist-list.jsx
+++ checklist-list/checklist-list.jsx
@@ -1,8 +1,11 @@
 export default function ReactChecklistList({ items }) {
-
-    return <ul className="item list-group">
-            {items.map((item, index) => (
-                        <li key={index} className="list-group-item">{item}</li>
-                    ))}
-            </ul>
+    return (
+        <ul className="item list-group">
+            {items
+                .filter(single => single && typeof single === 'object')
+                .map((single, index) => (
+                    <li key={index} className="list-group-item">{single.item}</li>
+                ))}
+        </ul>
+    );
 }
