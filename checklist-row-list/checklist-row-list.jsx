f9706388ac69c19293e8194c4ad5068c06e35bca
kevin <kwagner1312@gmail.com>
2026-09-12 07:50:45 -0500

    updates checlist rows to display in bootstrap accordion

--- checklist-row-list/checklist-row-list.jsx
+++ checklist-row-list/checklist-row-list.jsx
@@ -2,14 +2,18 @@ import ReactChecklistRow from '../checklist-row/checklist-row.js';
 
 export default function ReactChecklistRowsList({ checklists }) {
 
-    return checklists.map((checklist) => {
-        // Ensure items is an array
-        const items = Array.isArray(checklist.items) 
-            ? checklist.items 
-            : (typeof checklist.items === 'string' ? JSON.parse(checklist.items) : []);
-        
-        return (
-                <ReactChecklistRow items={items} checklist={checklist} key={checklist.id} />
-        );
-    });
+    return (
+        <div className="accordion" id="checklists-accordion">
+            {checklists.map((checklist) => {
+                // Ensure items is an array
+                const items = Array.isArray(checklist.items)
+                    ? checklist.items
+                    : (typeof checklist.items === 'string' ? JSON.parse(checklist.items) : []);
+
+                return (
+                        <ReactChecklistRow items={items} checklist={checklist} key={checklist.id} />
+                );
+            })}
+        </div>
+    );
 }
